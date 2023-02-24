b8cd61544cdea3d047a221771159ce61bbbfb563
WNIA <wnia22@gmail.com>
2023-02-24 12:12:48 -0800

    [WIP] header update (removed unnecessary parts and added color)

--- src/components/generalComponent/header.jsx
+++ src/components/generalComponent/header.jsx
@@ -2,35 +2,25 @@ import React from 'react';
 import './headerModule.css'
 
 const Header = () => {
-    const menus = [
-        {name: 'Login', link:'/'}
-    ]
     return(
         <div className='general header'>
-            <nav className='navbar navbar-expand-lgnavbar-light'>
-                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-targer='#navbarNav' aria-controls='navbarNav'>
-                    <span className='navbar-toggle-icon'>
-                        
-                    </span>
-                </button>
-                <div className='collapse navbar-collapse' id='navbarNav'>
-                    <ul className='navbar-nav'>
-                        {
-                            menus.map((menu) => {
-                                return(
-                                    <li className='nav-item'>
-                                        <a className='nav-link' href={menu.link}>{menu.name}</a>
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className='container-fluid'>
+            <nav className='navbar navbar-expand-lgnavbar-light custom-navbar'>
+                <div className='float-child'>
                     <a className='navbar-brand' href='/'>
                         <img src='images/summiLogo.png' className='header-logo' alt='logo'/>
                     </a>
                 </div>
+                <div className='float-child'>
+                    {/* <button className='header-login' color="inherit">Login</button> */}
+                    {/* <RaisedButton
+                        label="Submit"
+                        buttonStyle={{ borderRadius: 25 }}
+                        style={{ borderRadius: 25 }}
+                        labelColor={'#FFFFFF'}
+                        backgroundColor={"#0066e8"}
+    /> */}
+                </div>
+                
             </nav>
         </div>
     )
