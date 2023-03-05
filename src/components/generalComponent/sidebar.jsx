8d9028f111533f2ecb4810672b607c4a4c220fdd
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-03-04 17:41:15 -0800

    added media query for upload page

--- src/components/generalComponent/sidebar.jsx
+++ src/components/generalComponent/sidebar.jsx
@@ -5,7 +5,10 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Sidebar = () => {
   return (
-    <div className="col-auto col-md-2 bg-custom desktop">
+    <div
+      className="col-auto col-md-2 bg-custom desktop"
+      style={{ paddingLeft: "0", paddingRight: "0" }}
+    >
       <ul className="nav nav-pills flex-column">
         <li className="nav-item text-white fs-4">
           <a className="nav-link text-white fs-5" href="/" aria-current="page">
