da58398ee8a127edcb3185e8cdd1ee49f7d08d5d
WNIA <wnia22@gmail.com>
2023-03-30 01:27:53 -0700

    [WIP] login drop down update

--- src/components/generalComponent/loginDropDown.jsx
+++ src/components/generalComponent/loginDropDown.jsx
@@ -3,30 +3,29 @@ import './loginDropDown.css'
 
 
 const LoginDropDown = () => {
-    return(
-        <div className='loginDropDown'>
-            <ul className='loginDropDownItem'>
-            <div className='half-width white-bg'>
-          <br/>
+  return (
+    <div className='loginDropDown'>
+      <ul className='loginDropDownItem'>
+        <div className='half-width white-bg'>
           <h4>Login</h4>
-          <br/>
+          <br />
           <form action="">
             <div className='form-group'>
-              <input type="text" className='form-control' placeholder='Email'/>
+              <input type="text" className='form-control' placeholder='Email' />
             </div>
             <div className='form-group'>
-              <input type="password" className='form-control' placeholder='Password'/>
+              <input type="password" className='form-control' placeholder='Password' />
             </div>
-            <br/>
+            <br />
             <button type="submit" className='btn btn-primary right-btn'>Log in</button>
-            <br/>
-            <br>"Don't have an account? "</br>
-            <button type="submit" class="btn btn-primary right-btn">Sign up</button>
+            <br />
+            <br />
+            <p className='link'>Don't have an account? <a href="text-button">Sign up</a></p>
           </form>
-        </div>   
-            </ul>
         </div>
-    )
-} 
+      </ul>
+    </div>
+  )
+}
 
 export default LoginDropDown
