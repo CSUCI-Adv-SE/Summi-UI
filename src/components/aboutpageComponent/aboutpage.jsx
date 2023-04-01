5d2ca8406424b4971fa1244df185cc9abc5ea3e6
Shahin Safarov <tungsten.rust@gmail.com>
2023-03-31 20:43:24 -0700

    About Us page edited according to Sarah comment

--- src/components/aboutpageComponent/aboutpage.jsx
+++ src/components/aboutpageComponent/aboutpage.jsx
@@ -14,19 +14,29 @@ const AboutPage = () => {
     return (
         <div className="col">
             <h1>About Us</h1>
+            <h2>What is SummI?</h2>
             <p>
-                This Web App can read any PDF, JPEG, PNG file and can convert it to Text as an output.
-
+                “SummI” is a website that summarizes images. You can easily upload your image and receive the summary of the text in it.
             </p>
+            <h2>How does it work?</h2>
+            <p>
+                It uses artificial intelligence technologies to automatically recognize the text in the image and then summarize it.
+                Also, we enable you to upload any format of images you want, such as JPEG, PNG, WebP, etc.
+            </p>
+            <p>
+                We are five students studying MSc in computer science at CSUCI and participating in building this web application as a project for one of our classes.
+            </p>
+            <p>Thanks for choosing our website.</p>
             <p>Our team members:</p>
             <ul>
                 {teamMembers.map((member, index) => (
                     <li key={index}>{member}</li>
                 ))}
             </ul>
-            <img src={ocrFlowImage} alt="Our App Logic Flow" />
+            <img src={ocrFlowImage} alt="OCR flow diagram" className="image-50-percent" />
         </div>
     );
 };
 
-export default AboutPage;
+export default AboutPage
+
