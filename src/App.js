30c373ad113fce1948f2727cc9678b55a7b9f206
Shahin Safarov <tungsten.rust@gmail.com>
2023-03-31 22:15:20 -0700

    App js reverted back no changes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,14 @@ import "./App.css";
 import "react-notifications/lib/notifications.css";
 
 import HomePage from "./components/homepageComponent/homepage";
-import AboutPage from './components/aboutpageComponent/aboutpage';
-
 
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-        {/*<BrowserRouter>*/}
-        {/*    <Routes>*/}
-        {/*        <Route path="/about" element={<AboutPage />}>*/}
-        {/*        </Route>*/}
-        {/*    </Routes>*/}
-        {/*</BrowserRouter>*/}
-    </div>
-  );
+    return (
+        <div className="App">
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
+
