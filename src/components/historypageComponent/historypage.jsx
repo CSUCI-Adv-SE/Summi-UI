e64676a149472755ff5cb4d9ead5dc5cb0c3ca2d
Sarakhoshnoudrad <sara.khoshnoudrad890@myci.csuci.edu>
2023-04-15 18:15:35 -0700

    history page + example cards

--- src/components/historypageComponent/historypage.jsx
+++ src/components/historypageComponent/historypage.jsx
@@ -1,45 +1,39 @@
-import React, { useState, useEffect } from "react"
-import Header from "../generalComponent/header";
-import Sidebar from "../generalComponent/sidebar";
-import { Container, Table } from 'react-bootstrap';
-import "./table.css"
-import { config } from "../../constants.js";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-import './styles.css';
+import React from "react";
+import "./history.css"
+import Card from 'react-bootstrap/Card';
 
-const Table = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const response = await axios.get('https://example.com/data');
-    setData(response.data);
-  };
-
+function History() {
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Age</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.age}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <Card>
+        <Card.Img variant="top" src="./A_Little_Princess.jpeg" />
+        <Card.Body>
+          <Card.Text>
+            The little girl who lived in the story was named Lottie and she was interesting to many children. 
+            Some people who lived in the story were not interested in talking to my writer because they thought my writer was slouching, idle, and not interested in learning.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card>
+        <Card.Img variant="top" src="./image2.jpg" />
+        <Card.Body>
+          <Card.Text>
+            The woman is better than Picasso at something, so it is likely that she has failed at it more than him.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card>
+        <Card.Img variant="top" src="./image3.jpeg" />
+        <Card.Body>
+          <Card.Text>
+            We can't always need to worry about the people we don't matter near, but we always need to worry about the people we do matter near
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
   );
-};
+}
 
-export default Table;
+export default History;
