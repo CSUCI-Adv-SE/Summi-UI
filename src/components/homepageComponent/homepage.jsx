f05d4ffc3e4d838aedcd3c67dd9b0f0cd490044d
WNIA <wnia22@gmail.com>
2023-04-17 02:08:23 -0700

    [WIP] gistory page route fixed

--- src/components/homepageComponent/homepage.jsx
+++ src/components/homepageComponent/homepage.jsx
@@ -4,7 +4,7 @@ import UploadPageComponent from "../uploadFile/uploadpage";
 import { Component } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutPage from "../aboutpageComponent/aboutpage";
-import History from "../historypageComponent/historypage"
+import History from "../historypageComponent/historypage.jsx"
 
 class HomePage extends Component {
   state = {};
@@ -19,7 +19,7 @@ class HomePage extends Component {
               <Routes>
                 <Route path="/about" element={<AboutPage />}></Route>
                 <Route path="/" element={<UploadPageComponent />} />
-                <Route path="/history" element={<History />}></Route>
+                <Route path="/historypage" element={<History />}></Route>
               </Routes>
             </BrowserRouter>
           </div>
