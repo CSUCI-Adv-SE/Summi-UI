0b0e2da6c0b70dc6e6cdeb948acf8f32a770b256
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-04-26 16:20:50 -0700

    bug fixes

--- src/components/utils/passwordValidation.jsx
+++ src/components/utils/passwordValidation.jsx
@@ -1,7 +1,7 @@
 var passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
 
 function is_strong_password(s) {
-    return s.match(passwordPattern);
+    return s.test(passwordPattern);
 }
 
 export default is_strong_password;
