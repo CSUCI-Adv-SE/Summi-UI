15193012939d3c60f65e662bacef50bd06261ab2
Prashanth <prashanthkumarreddy.busupal523@myci.csuci.edu>
2023-04-26 16:13:44 -0700

    changed regex

--- src/components/utils/passwordValidation.jsx
+++ src/components/utils/passwordValidation.jsx
@@ -1,4 +1,4 @@
-var passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+var passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
 
 function is_strong_password(s) {
     return s.match(passwordPattern);
